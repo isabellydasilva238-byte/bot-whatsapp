e7bb514181b8468fa67bda95aa1459eda4c49668
isabellydasilva238-byte <isabellydasilva238@gmail.com>
2026-05-01 16:39:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Bot iniciado!");
+function responder(mensagem) {
+
+    if (mensagem === "!oi") {
+        return "Oiii 😄";
+    }
+
+    if (mensagem === "!menu") {
+        return `
+🤖 MENU
+
+!oi
+!menu
+!hora
+`;
+    }
+
+    if (mensagem === "!hora") {
+        return new Date().toLocaleTimeString();
+    }
+
+    return "Comando não encontrado";
+}
+
+console.log(responder("!menu"));
